a8d22deb9501ba7e2654e6fec0c0e4f5ba4f3fa6
namnguyen240498 <nam.nguyen@eastplayers.io>
2021-08-14 18:32:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
 import Login from "./components/Login";
+import ChatRoom from "./components/ChatRoom";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route components={Login} path="/login" />
+        <Route components={ChatRoom} path="/chat" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Row, Col, Button, Typography } from "antd";
 import firebase, { auth } from "../../services/firebase/config";
+import { useHistory } from "react-router-dom";
 
 const { Title } = Typography;
 
-const fbProvider = firebase.auth.FacebookAuthProvider();
+const fbProvider = new firebase.auth.FacebookAuthProvider();
 
 export default function Login() {
+  const history = useHistory();
   const handleFBLogin = () => {
     auth.signInWithPopup(fbProvider);
   };
+  auth.onAuthStateChanged((user) => {
+    console.log("user", user);
+    if (user) {
+      history.push("/chat");
+    }
+  });
   return (
     <div>
       <Row justify="center" style={{ height: 800 }}>
@@ -18,13 +26,13 @@ export default function Login() {
             Simple Chat
           </Title>
           <Button style={{ width: "100%", marginBottom: 5 }}>
-            Đăng nhập bằng Facebook
+            Đăng nhập bằng Google
           </Button>
           <Button
             style={{ width: "100%", marginBottom: 5 }}
             onClick={handleFBLogin}
           >
-            Đăng nhập bằng Google
+            Đăng nhập bằng Facebook
           </Button>
         </Col>
       </Row>
